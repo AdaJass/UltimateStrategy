5a3c8b0bb0e50a99ac133b765e32bdff874d2ff8
adajass <jass.ada@qq.com>
2018-02-28 00:01:51 +0800

    kjjk

---
+++ UltiS0/recognizor.py
@@ -0,0 +1,6 @@
+"""to make it easy, this recognizor will just use OHLC and volume as 
+its patterns.
+X={
+    
+}
+"""
---
+++ UltiS0/saver.py
@@ -0,0 +1,24 @@
+"""this file help to definde and save usable and redeay used pattern.abs
+"""
+import pickle
+
+usableData = {
+    'data' = [],
+    'dataLen' =0,
+    'flurMethod' = '',
+    'flurPara' = 0
+}
+
+preData = {
+    'data' = [],
+    'dataLen' =0,
+    'flurMethod' = '',
+    'flurPara' = 0
+}
+
+def readData(path='./patterns'):
+
+    pass
+
+def writeDate(path='./patterns', usable=True):
+    pass
